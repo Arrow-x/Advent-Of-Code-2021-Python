25c34ff6608f2d790f1599ec8031efff831974b4
Arrow-x <Houssem.kun26@gmail.com>
2022-05-11 02:41:54 +0100

    Day 3 Challenge part 2

--- day3.py
+++ day3.py
@@ -2,12 +2,74 @@ import fileinput
 
 
 def main():
+    # part_one(fileinput.input())
+    co2_list = list(fileinput.input())
+    oxygen_list = list(fileinput.input())
+
+    oxygen_gen_byte = ""
+    co2_scr_byte = ""
+    len_of_byte = len(oxygen_list[0])
+
+    for i in range(len_of_byte):
+        if len(oxygen_list) == 1:
+            oxygen_gen_byte = oxygen_list[0]
+            break
+
+        ones_list = []
+        zeros_list = []
+        for line in oxygen_list:
+            if line[i] == "1":
+                ones_list.append(line)
+            if line[i] == "0":
+                zeros_list.append(line)
+
+        if len(ones_list) > len(zeros_list):
+            oxygen_list = ones_list
+        if len(ones_list) == len(zeros_list):
+            oxygen_list = ones_list
+        if len(ones_list) < len(zeros_list):
+            oxygen_list = zeros_list
+
+    for i in range(len_of_byte):
+        if len(co2_list) == 1:
+            co2_scr_byte = co2_list[0]
+            break
+
+        ones_list = []
+        zeros_list = []
+
+        for line in co2_list:
+            if line[i] == "1":
+                ones_list.append(line)
+            if line[i] == "0":
+                zeros_list.append(line)
+
+        if len(ones_list) > len(zeros_list):
+            co2_list = zeros_list
+        if len(ones_list) == len(zeros_list):
+            co2_list = zeros_list
+        if len(ones_list) < len(zeros_list):
+            co2_list = ones_list
+
+    o2_gen_decimal = int(oxygen_gen_byte, 2)
+    co2_scrub_decimal = int(co2_scr_byte, 2)
+    print(
+        "Oxygen Generator byte is",
+        oxygen_gen_byte,
+        "it's decimal is",
+        o2_gen_decimal,
+    )
+    print("CO2 Scrubber byte is", co2_scr_byte, "it's decimal is", co2_scrub_decimal)
+    print("Life support rating is", o2_gen_decimal * co2_scrub_decimal)
+
+
+def part_one(files):
     gamma_rate_byte = [0] * 12
     epsilon_rate_byte = [0] * 12
     zero_count = [0] * 12
     one_count = [0] * 12
 
-    for line in fileinput.input():
+    for line in files:
         bits = list(str(line.rstrip()))
         for b in range(len(bits)):
             if int(bits[b]) == 1:
