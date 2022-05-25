63d8f32c32fc43c7eb6a215c80bfa7465a1c4c60
Arrow-x <Houssem.kun26@gmail.com>
2022-05-25 17:53:30 +0100

    Deleting duplicate code

--- day5/day5.py
+++ day5/day5.py
@@ -7,49 +7,10 @@ def main():
     for i in range(len(point_map)):
         point_map[i] = [0]*data_len
     data.close()
-    part2(point_map, nonempty_lines, data_len)
+    parser(point_map, nonempty_lines, data_len)
 
 
-def part1(point_map, nonempty_lines, data_len):
-    for line in nonempty_lines:
-        line = line.replace(" -> ", ",")
-        line = line.rstrip().split(",")
-        line = list(map(int, line))
-
-        x1 = line[0]
-        x2 = line[1]
-        y1 = line[2]
-        y2 = line[3]
-
-        if x2 == y2:
-            ll = [0]*data_len
-            if x1 < y1:
-                for i in range(x1, y1+1):
-                    ll[i] += 1
-            if x1 > y1:
-                for i in range(y1, x1+1):
-                    ll[i] += 1
-
-            sumlist = [a + b for a, b in zip(point_map[x2], ll)]
-            point_map[x2] = sumlist
-
-        if x1 == y1:
-            if x2 < y2:
-                for i in range(x2, y2+1):
-                    point_map[i][x1] += 1
-            if x2 > y2:
-                for i in range(y2, x2+1):
-                    point_map[i][x1] += 1
-
-    overlap_count = 0
-    for i in point_map:
-        for b in i:
-            if b >= 2:
-                overlap_count += 1
-    print("Vertical and Horizantal line overlap", overlap_count, "Times!")
-
-
-def part2(point_map, nonempty_lines, data_len):
+def parser(point_map, nonempty_lines, data_len):
     for line in nonempty_lines:
         line = line.replace(" -> ", ",")
         line = line.rstrip().split(",")
@@ -70,7 +31,6 @@ def part2(point_map, nonempty_lines, data_len):
                     ll[i] += 1
 
             point_map[x2] = [a + b for a, b in zip(point_map[x2], ll)]
-            continue
 
         if x1 == y1:
             if x2 < y2:
@@ -79,18 +39,21 @@ def part2(point_map, nonempty_lines, data_len):
             if x2 > y2:
                 for i in range(y2, x2+1):
                     point_map[i][x1] += 1
-            continue
 
         if abs(y1 - x1) == abs(y2 - x2):
             mark_diag(x1, y1, x2, y2, point_map, y1 - x1, y2 - x2)
 
+    overlaps(point_map)
+
+
+def overlaps(point_map):
     overlap_count = 0
     for i in point_map:
         for b in i:
             if b >= 2:
                 overlap_count += 1
 
-    print("Vertical Horizantal and diagonal lines overlap", overlap_count, "Times!")
+    print("The lines overlap", overlap_count, "Times!")
 
 
 def mark_diag(x1, x2, y1, y2, point_map, dir_x, dir_y):
